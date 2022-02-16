6c31aa6cf292b38f6f6d604f093f6b9c089eefd4
giovanni <144000giovanni@gmail.com>
2022-02-17 12:39:13 +1300

    added subfolders

--- addImages.py
+++ addImages.py
@@ -7,24 +7,85 @@ import requests
 
 folder = input("category: ")
 
-while True:
-  url = input("url: " )
-  totalFiles = len(os.listdir(folder))
-  print("totalFiles: " + str(totalFiles))
+
+def createFolder(folder):
+   totalSubFolders = len(os.listdir(folder))
+   newFolder = totalSubFolders + 1
+   newFolderString = str(newFolder)
+   os.mkdir("./"+folder + "/" + newFolderString)
+   return newFolderString
+
+def createFileName(subfolder):
+  subfolderlist = os.listdir("./" + folder + "/" + subfolder)
+  totalFiles = len(subfolderlist)
+  fileIndex = str(totalFiles)
+  return subfolder + "_" + folder + fileIndex + ".png"
+  
+def getLatestSubFolderName(mainFolder):
+  print("mainFolder: " + str(mainFolder))
+  totalFolders = len(mainFolder)
+  lastFolderSetIndex = totalFolders - 1
+  lastSubFolder = mainFolder[lastFolderSetIndex]
+  return lastSubFolder
+
+def getImage(url):
   response = requests.get(url, stream=True)
-  filename = folder + str(totalFiles) + ".png"
-  with open("./" + folder +"/"+filename, 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
+  return response  
+
+def getPath(folder, subfolder, filename):
+   return "./" + folder + "/" + subfolder + "/" + filename
+   
+def getGithubFilname(folder, subfolder, filename):  
+  githuburl = "https://github.com/gesab001/images/raw/main/" + folder + "/" + subfolder + "/"+ filename
+  return githuburl
+ 
+def saveImage(path, response):
+  with open(path, 'wb') as out_file:
+      shutil.copyfileobj(response.raw, out_file)     
+
+def getTotalFilesOfSubFolder(subfolder):
+    subfolderlist = os.listdir("./" + folder + "/" + subfolder)
+    totalFiles = len(subfolderlist)
+    return totalFiles
+while True:
+  input("test")
+  url =  input("url: " ) #"https://a.cdn-hotels.com/gdcs/production138/d1824/ed0d5995-0902-43d1-9644-1aa7a1a27d81.jpg"
+  response = getImage(url)
+  
+  mainFolder = os.listdir(folder)
+  subFolderName = getLatestSubFolderName(mainFolder)
+  filename = createFileName(subFolderName)
+  totalFiles = getTotalFilesOfSubFolder(subFolderName)
+  if totalFiles>=10:
+    subFolderName = createFolder(folder) 
+    filename = createFileName(subFolderName)
+  path = getPath(folder, subFolderName, filename)  
+  saveImage(path, response)
   del response
   fileimage = open("imagesList.json", "r")
   jsonObj = json.loads(fileimage.read())
   fileimage.close()
-  githuburl = "https://github.com/gesab001/images/raw/main/" + folder + "/" + filename
+  githuburl = getGithubFilname(folder, subFolderName, filename)
   if folder in jsonObj:
     print("Key exists")
-    jsonObj[folder].append(githuburl)
+    imagesets = list(jsonObj[folder].keys()) #["1", "2"]
+    print("imagesets: " + str(imagesets))
+    totalSets = len(imagesets) #number of keys
+    print("totalSets: " + str(totalSets))
+    lastSetIndex = totalSets - 1
+    print("lastSetIndex: " + str(lastSetIndex))
+    lastSetKey = imagesets[lastSetIndex] #last list in a set
+    print("lastSetKey: " + str(lastSetKey))
+    totalPicsInASet = len(jsonObj[folder][lastSetKey]) #list of images ["image", "image"]
+    print("totalPicsInASet: " + str(totalPicsInASet))
+    if totalPicsInASet!=10:
+      jsonObj[folder][lastSetKey].append(githuburl)
+    else:
+      nextSetIndex = str(int(lastSetKey) + 1)
+      jsonObj[folder][nextSetIndex] = [githuburl]
+   
   else:
    print("Key does not exist")
-   jsonObj[folder] = [githuburl]
+   jsonObj[folder]["1"] = [githuburl]
   with open("imagesList.json", 'w') as out_file:
     json.dump(jsonObj, out_file, indent=4, sort_keys=True)
